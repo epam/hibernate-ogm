84a7d59404709ed3137d4c60d115d9feef9a6eef
Artem Laushkin <Artem_Laushkin@epam.com>
2018-04-19 16:24:27 +0300

    OGM-1349-modify-neo4j-test-procedure

--- neo4j/src/test/java/org/hibernate/ogm/datastore/neo4j/test/procedures/CarStoredProcedures.java
+++ neo4j/src/test/java/org/hibernate/ogm/datastore/neo4j/test/procedures/CarStoredProcedures.java
@@ -1,10 +1,5 @@
 package org.hibernate.ogm.datastore.neo4j.test.procedures;
 
-import static org.neo4j.kernel.api.proc.Neo4jTypes.*;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.stream.Stream;
 
 import org.hibernate.ogm.backendtck.storedprocedures.Car;
@@ -19,34 +14,30 @@ public class CarStoredProcedures {
 
 	@Procedure(value = Car.SIMPLE_VALUE_PROC, mode = Mode.WRITE)
 	public Stream<Result> simpleValueProcedure(@Name("id") long id) {
-		return Collections.list( new Result( id, "" ) ).stream();
+		return Collections.list( new Result( (int) id, null ) ).stream();
 	}
 
 	@Procedure(value = Car.RESULT_SET_PROC, mode = Mode.WRITE)
 	public Stream<Result> resultSetProcedure(@Name("id") long id, @Name("title") String title) {
-		return Collections.list( new Result( id, title ) ).stream();
+		return Collections.list( new Result( (int) id, title ) ).stream();
 	}
 
 	public static class Result {
-		public long id;
+
+		public Number id;
 		public String title;
 
 		public Result() {
 		}
 
-		public Result(Long id) {
+		public Result(Integer id) {
 			this.id = id;
 		}
 
-		public Result(Long id, String title) {
+		public Result(Integer id, String title) {
 			this.id = id;
 			this.title = title;
 		}
 	}
+
 }
-//	Result result = dataBase.execute( "CALL dbms.procedures()" );
-//	ArrayList list = new ArrayList(  );
-//while(result.hasNext()){
-//		list.add( result.next().get( "name" ) );
-//		}
-//		list
